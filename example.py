a7e1de2c23850a7b154495194ef3f8cd9ec51673
Shep Bryan IV <shepperoni.pizza@gmail.com>
2022-09-29 15:47:30 -0700

    Add files via upload

--- example.py
+++ example.py
@@ -7,33 +7,25 @@ from fret_analyzer_HMM import FRETAnalyzerHMM
 
 # Set parameters
 file = 'exampledata.h5'
-savename = 'exampleresults.h5'
 num_data = 1000
-num_iter = 10
+parameters = {
+    'dt': 1e6,    # Time step in nanoseconds
+    'kT': 4.114,  # Temperature in kT
+    'R0': 5,      # Characteristic FRET distance in nanometers
+}
 
 # Load data
 h5 = h5py.File(file, 'r')
 data = h5['data'][()]
-parameters = {key: h5[key][()] for key in h5.keys() if key != 'data'}
 h5.close()
-
-# Crop data
 data = data[:, :num_data]
-if 'traj_mask' in parameters:
-    parameters['traj_mask'] = parameters['traj_mask'][:num_data]
 
 # Run analysis
-MAP, history = FRETAnalyzer.learn_potential(
-    data, parameters=parameters, num_iter=num_iter, saveas=savename,
-)
+MAP, history = FRETAnalyzer.learn_potential(data, parameters=parameters)
+FRETAnalyzer.plot_variables(data, MAP)
 
 # Run analysis HMM
-MAP_HMM, history_HMM = FRETAnalyzerHMM.learn_potential(
-    data, parameters=parameters, num_iter=num_iter, saveas=savename+'_HMM',
-)
-
-# Plot results
-FRETAnalyzer.plot_variables(data, MAP)
+MAP_HMM, history_HMM = FRETAnalyzerHMM.learn_potential(data, parameters=parameters)
 FRETAnalyzerHMM.plot_variables(data, MAP_HMM)
 
 
